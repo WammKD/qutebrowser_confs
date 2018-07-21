e4b4c4e83beade6f14626ada48bfa3275c5dcbee
Wamm K. D <jaft.r@outlook.com>
2018-07-21 13:40:21 -0500

    Add in a custom stylesheet to make DuckDuckGo a little more bearable to use.

--- config.py
+++ config.py
@@ -746,7 +746,7 @@ c.content.plugins = True
 
 ## List of user stylesheet filenames to use.
 ## Type: List of File, or File
-# c.content.user_stylesheets = []
+c.content.user_stylesheets = ["~/.config/qutebrowser/stylesheets/duckduckgo.css"]
 
 ## Enable WebGL.
 ## Type: Bool
